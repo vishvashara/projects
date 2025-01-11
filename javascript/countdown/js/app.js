892d25a6a5a12b9aa952fbf8bc2fbfbf0797ad1d
vishvashara <asharavishv238@gmail.com>
2025-01-11 12:16:05 +0530

    first commit

--- javascript/countdown/js/app.js
+++ javascript/countdown/js/app.js
@@ -1,37 +1,29 @@
-const endDate = "8 June 2024 08:00 PM"
-
+const endDate ="1 january 2025 10:00 PM"
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
-    // const clock = () => {
 
-// }
-
-function clock() {
+function clock(){
     const end = new Date(endDate)
     const now = new Date()
-    const diff = (end - now) / 1000;
-
-    if (diff < 0) return;
-
-    // convert into days;
-    inputs[0].value = Math.floor(diff / 3600 / 24);
-    inputs[1].value = Math.floor(diff / 3600) % 24;
-    inputs[2].value = Math.floor(diff / 60) % 60;
-    inputs[3].value = Math.floor(diff) % 60;
-}
-
+    const diff = (end - now)/1000;  
+    if(diff<0) return;
+    inputs[0].value=Math.floor(diff/3600/24)
+    inputs[1].value=Math.floor(diff/3600)%24
+    inputs[2].value=Math.floor(diff/60)%60      
+    inputs[3].value=Math.floor(diff)%60
+}    
 // initial call
-clock()
+clock(); 
+
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+)
 
 /**
  *  1 day = 24 hrs
  *  1 hr = 60 mins
  *  60 min = 3600 sec
  */
-
-setInterval(
-    () => {
-        clock()
-    },
-    1000
-)
